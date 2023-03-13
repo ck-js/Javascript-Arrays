f3e3129ff2a9d50f32e1c962e07554b917fa0bbe
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-13 15:59:02 +0700

    Add strains array with if else statement depending on the array length to output a particular message then created a for loop to display the arrays contenets then used the push and pop array methods to add and remove values to the last index of our array. Also note our if else statement comes into play as the array length shortens or increases

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 const cars= ["Honda", "Toyota", "Lexus"]
 const firstCar = cars[0]; 
 console.log(`The first car is a ${firstCar}`);
+
+const passCode = [5, 2, 2, 6];
+console.log(passCode);
+passCode[0] = 3;
+passCode[3] = 5;
+console.log(passCode);
+
+const strains = ["SGV", "Northern Lights", "Durban Poisan", "Laughing Buddha"];
+strains.push("Wedding Cake", "Bubblegum");
+let removeStrain = strains.pop();
+console.log(removeStrain + "just sold out") 
+const strainsLength = strains.length;
+if (strainsLength <= 5) {
+console.log("You have " + strainsLength +
+ " strains left, time to stock up");
+
+} else {
+    console.log("You have " + strainsLength +
+    " strains in stock, no need to stock up");
+}
+for (let i = 0; i < strainsLength; i++){
+console.log(strains[i]);
+}
+
